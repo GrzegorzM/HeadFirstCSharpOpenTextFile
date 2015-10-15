14a2082b3436bb5b442e2f025953d32330b2db91
Kesiu <kesiu.pro@gmail.com>
2015-10-15 02:07:16 +0200

    Running program.

---
+++ HeadFirstCSharpOpenTextFile/HeadFirstCSharpOpenTextFile/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadFirstCSharpOpenTextFile
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetDialogBoxesProperties();
+        }
+
+        private void SetDialogBoxesProperties()
+        {
+            saveFileDialog1.InitialDirectory = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|" + "Comma-Delimited Files (*.csv)|*.csv|All Files (*.*)|*.*";
+
+            openFileDialog1.InitialDirectory = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+            openFileDialog1.Filter = "Text Files (*.txt)|*.txt|" + "Comma-Delimited Files (*.csv)|*.csv|All Files (*.*)|*.*";
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(name);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = saveFileDialog1.FileName;
+                File.WriteAllText(name, textBox1.Text);
+            }
+        }
+    }
+}
